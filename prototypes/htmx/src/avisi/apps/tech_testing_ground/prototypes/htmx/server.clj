31ab5d6a94b4a42bf5e83efa07defe071f30a26a
Jaap Maaskant <jaapmaaskant@hotmail.com>
2023-03-15 14:13:00 +0100

    integrated htmx-impl with iframe, too easy

--- prototypes/htmx/src/avisi/apps/tech_testing_ground/prototypes/htmx/server.clj
+++ prototypes/htmx/src/avisi/apps/tech_testing_ground/prototypes/htmx/server.clj
@@ -2,13 +2,15 @@
   (:require
     [avisi.apps.tech-testing-ground.prototypes.htmx.hello-world :as hello-world]
     [avisi.apps.tech-testing-ground.prototypes.shared.server :as server]
-    [mount.core :as mount :refer [defstate]]))
+    [mount.core :as mount :refer [defstate]]
+    [ring.util.response :refer [redirect]]))
 
 (def routes (hello-world/routes))
 
 (def server-config
   {:port (server/get-port "htmx")
-   :routes routes})
+   :routes routes
+   :jira-handlers {:issue-panel-handler (constantly (redirect "iframe-content.html"))}})
 
 (defn start-server [] (server/start-server server-config))
 
